4a75a0c39901d26d121814411e5c1d9037030440
florianc <castelainflorian44@gmail.com>
2023-06-25 09:35:50 +0200

    feat(#4): gen sun, planet, asteroid

---
+++ generator.js
@@ -0,0 +1,94 @@
+let factory_instance = null;
+
+class Factory{
+
+    constructor() {
+        if (factory_instance == null) {
+            factory_instance = this
+        } 
+        return this
+    }
+
+    // generate_system(x, y, name, id, info=null) {
+    //     const m = L.marker(
+    //         [x, y],
+    //         {
+    //             icon: this.ss_icon,
+    //             title: name
+    //         }
+    //     )
+    //     const popup = `${name} (${x}:${y})<br><button onclick="location.href='/system/${id}'">visit</button>`
+    //     m.bindPopup(popup)
+    //     m["info"] = info
+    //     return m
+    // }
+
+    generate_planet(x, y, name, radius) {
+        const c = L.circle(
+            [x, y], {
+                radius: radius
+            }
+        )
+        // x = x.toFixed(2)
+        // y = y.toFixed(2)
+        return c
+    }
+
+    generate_sun(x, y, name, radius) {
+        const c = L.circle(
+            [x, y], {
+                radius: radius,
+                fillColor: "yellow",
+                color: "yellow"
+            }
+        )
+        return c
+    }
+
+    generate_asteroid(x, y, name, radius) {
+        const a = L.circle(
+            [x, y], {
+                radius: radius,
+                fillColor: "black",
+                color: "black"
+            }
+        )
+        return a
+    }
+
+    // speed: unit per sec
+    // generate_ship(x, y, name, speed) {
+    //     const popup = `
+    //     ${name}
+    //     `
+    //     const s = L.marker([x, y])
+    //     s.speed = speed
+    //     s.bindPopup(popup)
+    //     s.on("click", new MapManager().click_ship)
+    //     return s
+    // }
+
+    // generate_system(x, y, planet_qtt, name) {
+    //     const sun = this.generate_sun(x,y, name, new MapManager().sun_radius)
+    //     let planets = []
+    //     for (let i = 1; i<=planet_qtt; i++) {
+    //         const dist = new MapManager().distance_from_object_with_orbit(
+    //             new MapManager().sun_radius,
+    //             i,
+    //             new MapManager().orbit_interval_len
+    //         )
+    //         let angle = Math.random()*Math.PI*2
+    //         let px = Math.cos(angle)*dist + x
+    //         let py = Math.sin(angle)*dist + y
+    //         let p = new Factory().generate_planet(
+    //             px, py, name + "-" + i, 1
+    //         )
+    //         console.log(px, py, dist)
+    //         planets.push(p)
+    //     }
+    //     return {
+    //         's': sun,
+    //         'p': planets
+    //     }
+    // }
+}
---
+++ map_manager.js
@@ -0,0 +1,94 @@
+let map_manager = null;
+
+class MapManager {
+
+    constructor() {
+        if (map_manager == null) {
+            map_manager = this
+            this.map = L.map('map', {
+                crs: L.CRS.Simple
+            });
+            var bounds = [[0,0], [1000,1000]];
+            L.imageOverlay('empty_map.png', bounds).addTo(this.map);
+            this.map.fitBounds(bounds);
+            this.map_objects = {
+                "stars": {},
+                "ships": {}
+            }
+            this.map.on("click", this.click_map)
+        }
+        return this
+    }
+
+    // ################################################### handle map stuff
+    sun_radius = 10
+    orbit_interval_len = 5
+
+    add_to_map(element) {
+        element.addTo(map_manager.map)
+    }
+
+    calculate_move_duration(src_latLng, dest_LatLng, speed) {
+        let d = map_manager.map.distance(
+            src_latLng,
+            dest_LatLng
+        )
+        return d / parseFloat(speed)
+        
+    }
+
+    // calcul distance aléatoire à partir d'un rayon, d'un coef et de la largeur d'une orbite
+                    // | ceci * orbit_interval |
+    // . --radius--> . --largeur orbite-->
+    distance_from_object_with_orbit(source_radius, orbit_interval, orbit_interval_len) {
+        let low = source_radius + orbit_interval * orbit_interval_len
+        let high = low + orbit_interval_len
+        let r = new Utils().random_float_from_interval(low, high)
+        return r
+    }
+
+
+    travel_duration(x_origin, y_origin, x_dest, y_dest, speed) {
+        let distance = map_manager.map.distance(
+            L.latLng(x_origin, y_origin),
+            L.latLng(x_dest, y_dest)
+        )
+        return distance/speed * 1000
+    }
+
+
+    // ################################################### handle context
+    selected_element = null
+    click_ship(mouse_event) {
+        map_manager.selected_element = mouse_event.sourceTarget
+    }
+
+
+    click_map(mouse_event) {
+        if (map_manager.selected_element != null) {
+            const x = mouse_event.latlng.lat
+            const y = mouse_event.latlng.lng
+            let duration = map_manager.travel_duration(
+                map_manager.selected_element.getLatLng().lat,
+                map_manager.selected_element.getLatLng().lng,
+                mouse_event.latlng.lat,
+                mouse_event.latlng.lng,
+                map_manager.selected_element.speed
+            )
+            map_manager.selected_element.slideTo([x, y], {duration: duration})
+            map_manager.selected_element = null
+        }
+    }
+
+    stop_selected_element() {
+        if (map_manager.selected_element != null) {
+            map_manager.selected_element.slideCancel()
+        }
+    }
+
+    unselect() {
+        map_manager.selected_element = null
+    }
+}
+
+new MapManager()
